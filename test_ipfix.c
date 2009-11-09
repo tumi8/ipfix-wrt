0e0348521aabb440653afc0d610a594b1999e50d
kamy <kamy@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2009-11-09 20:46:33 +0000

    current state of the work
    git-svn-id: file:///Users/braun/svn/vermont/branches/ipfix-wrt@2182 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- test_ipfix.c
+++ test_ipfix.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <regex.h>
+#include <time.h>
 
 //Variablen für die größe der einzelnen sendefenster/buffer
 #define SEND_BUFFER_SIZE 1024
@@ -14,10 +15,11 @@ char send_buffer[SEND_BUFFER_SIZE];
 regmatch_t match_buffer[MATCH_BUFFER_SIZE];
 int send_buffer_offset = 0;
 ipfix_exporter *send_exporter;
+uint16_t my_template_id = 12345;
 
 /*
- * Das zentrale rule struct beschreibt die regel zur umwandlung einer capturing
- * group:
+ * Das zentrale rule struct beschreibt die Regel zur Umwandlung einer Capturing
+ * Group:
  * Der bytecount gibt an wieviel byte der wert nach der konvertierung im sendbuffer belegen wird
  * transform muss eine funktion sein, die die eingabe (char*) und den sendbuffer (void*)
  * erhält, die eingabe geeignet konvertiert (z.B. in int umwandeln, networkbyteorder konvertierungen usw)
@@ -27,39 +29,65 @@ ipfix_exporter *send_exporter;
  * im bytecount angegeben sind.
  */
 typedef struct {
-	int bytecount;
-	void (*transform)(char*,void*);
-
+	uint16_t bytecount;
+	void (*transform)(char* input,void* target_buffer, transform_rule* rule);
+	uint16_t type;
+	uint32_t enterprise_id;
 } transform_rule;
 
 /******* IMPLEMENTIERTE REGELN ***********/
 /**
- * Hier sind einige beispielregeln implementiert.
- * Zuerst muss jeweils die transformationsfunktion definiert werden
- * welche dann im struct als pointer referenziert werden kann.
+ * Hier sind einige Beispielregeln implementiert.
+ * Zuerst muss jeweils die Transformationsfunktion definiert werden
+ * welche dann im struct als Pointer referenziert werden kann.
  */
 
 //Transformation in 4 byte signed int
-void transform_int(char* input, void* buffer){
+void transform_int(char* input, void* buffer, transform_rule* rule){
 	int i = atoi(input);
 	(*(int*)buffer) = i;
-
+    // der Int-Wert soll in der Adresse gespeichert werden, wo der Zeiger zeigt..
+	//(int*)buffer -> es muss angegeben werden, was für Daten in dem Buffer gespeichert werden
+	//je nachden wo der Zeiger zeigt (offset) wird dort der Int Wert gespeichert
+}
+void transform_string(char* input, void* buffer, transform_rule* rule){
+	char* c = input;
+	//(*(char*)buffer) = &c;
+    // der Int-Wert soll in der Adresse gespeichert werden, wo der Zeiger zeigt..
+	//(int*)buffer -> es muss angegeben werden, was für Daten in dem Buffer gespeichert werden
+	//je nachden wo der Zeiger zeigt (offset) wird dort der Int Wert gespeichert
 }
 
-transform_rule rule_int = {4,&transform_int}; //Transformiert in 4 byte int per atoi
-transform_rule rule_null = {0,0}; //Ignoriert diesen wert
+transform_rule rule_ip_source = {4,&transform_int,}; //Transformiert in 4 byte int per atoi
+transform_rule rule_ignore = {0,0,0,0}; //Ignoriert diesen wert
+
+//transform_rule rule_string = {1,&transform_string};
+//transform_rule rule_ip = {1,&transform_ip};
 
 /******* IMPLEMENTIERTE REGELN ENDE ******/
 
+void rules_to_template(uint16_t template_n_id,transform_rule* rules, int num_rules){
+
+	ipfix_start_template_set(send_exporter, template_n_id, num_rules);
+	for(int i=0;i<num_rules;i++){
+		//Skip rules with bytecount 0
+		if(rules[i].bytecount == 0) continue;
+
+		ipfix_put_template_field(send_exporter, template_n_id, rules[i].type, rules[i].bytecount, rules[i].enterprise_id);
+	}
+	ipfix_end_template_set(send_exporter, template_n_id);
+
+
+}
 //Wendet eine regel auf einen inputstring an
-//Die transform funktion der Regel sollte(!) den string geeignet umwandeln und
-//ihn dann an die position schreiben, die als zweiter parameter übergeben wurde.
+//Die Transformfunktion der Regel sollte(!) den String geeignet umwandeln und
+//ihn dann an die Position schreiben, die als zweiter Parameter übergeben wurde.
 //Danach schiebt apply_rule den offset um bytecount weiter, sodass der neue offset
-//wieder hinter den belegten Speicher im send buffer zeigt.
+//wieder hinter den belegten Speicher im send_buffer zeigt.
 void apply_rule(char* input,transform_rule* rule){
 
 	//Regel anwenden
-	rule->transform(input,send_buffer+send_buffer_offset);
+	rule->transform(input,send_buffer+send_buffer_offset,rule);
 
 	//Offset weiterschieben
 	send_buffer_offset+=rule->bytecount;
@@ -68,12 +96,12 @@ void apply_rule(char* input,transform_rule* rule){
 //Nimmt eine line <input> (z.B. aus einer proc datei)
 //wendet das übergebene pattern <regEx> auf sie an
 //Die ersten <num_rules> matches werden mit den übergebenen <rules>
-//transformiert und in den globalen send buffer geschrieben.
-//<rules> muss dabei ein array aus transform_rules sein mit (mindestens) <num_rules> Einträgen
-//Jede rule aus dem array wird für eine capturing group des patterns verwendet
-//Soll eine capturing group gar nicht gesendet werden, muss einfach an der jeweiligen
-//stelle im array eine regel mit .bytecount==0 stehen
-//Die funktion returned true, falls die Zeile gematcht werden konnte (pattern traf zu)
+//transformiert und in den globalen send_buffer geschrieben.
+//<rules> muss dabei ein array aus *transform_rules sein mit (mindestens) <num_rules> Einträgen
+//Jede rule aus dem array wird für eine Capturing Group des Patterns verwendet
+//Soll eine Capturing Group gar nicht gesendet werden, muss einfach an der jeweiligen
+//Stelle im array eine Regel mit .bytecount==0 stehen
+//Die Funktion gibt true zurück, falls die Zeile gematcht werden konnte (Pattern traf zu)
 //ansonsten false
 int line_to_send_buffer(char* input, regex_t* regEx,transform_rule* rules, int num_rules){
 
@@ -83,25 +111,23 @@ int line_to_send_buffer(char* input, regex_t* regEx,transform_rule* rules, int n
 	//Mit pattern matchen
 	if(!regexec(regEx, input,num_rules+1, match_buffer, 0)){
 		printf("%s\n",input);
-		//Über alle capturing groups/rules iterieren
+		//Über alle Capturing Groups/Rules iterieren
 		int i;
-		for( i=1; i <= num_rules; i++){
+		for(i=1; i <= num_rules; i++){
 
-			//Nur was tun, wenn die regel bytecount > 0
+			//Nur was tun, wenn die Regel bytecount > 0
 			if(rules[i-1].bytecount!=0){
 
-				//Jede capturing group erst nullterminieren, dass sie als eingabestring benutzt werden kann
+				//Jede Capturing Group erst nullterminieren, dass sie als Eingabestring benutzt werden kann
 				//Dabei das durch \0 ersetzte Zeichen merken, dass wir es später wieder tauschen können
 				char swap = input[match_buffer[i].rm_eo]; //merken
 				input[match_buffer[i].rm_eo]='\0'; //0 terminieren
 
-				//Regel anwenden! (dies transformiert den inhalt und schreibt ihn in den Sendbuffer)
+				//Regel anwenden! (dies transformiert den Inhalt und schreibt ihn in den send_buffer)
 				apply_rule(&input[match_buffer[i].rm_so],&rules[i-1]);
 
 				//Nullterminierung rückgängig machen
 				input[match_buffer[i].rm_eo] = swap;
-
-
 			}
 
 		}
@@ -113,13 +139,13 @@ int line_to_send_buffer(char* input, regex_t* regEx,transform_rule* rules, int n
 }
 
 /**
- * Sendet einfach den kompletten sendepuffer als ein datensatz an ipfix.
+ * Sendet einfach den kompletten send_buffer als ein Datensatz an ipfix.
  *
  * Diese funktion kann/sollte noch verändert werden, sodass sie mehrere
  * datensätze aufeinmal schicken kann.
  *
- * Bis jetzt wird nämlich nach jedem datensatz sofort ein send aufgerufen,
- * was unnötigen paketoverhead bedeuten könnte.
+ * Bis jetzt wird nämlich nach jedem Datensatz sofort ein send() aufgerufen,
+ * was unnötigen Paketoverhead bedeuten könnte.
  *
  */
 int dump_buffer_to_ipfix(uint16_t template_n_id){
@@ -129,7 +155,6 @@ int dump_buffer_to_ipfix(uint16_t template_n_id){
 	if (ret != 0 ) {
 		fprintf(stderr, "ipfix_start_data_set failed!\n");
 	} else {
-
 		ipfix_put_data_field(send_exporter, send_buffer, send_buffer_offset);
 		ret=ipfix_end_data_set(send_exporter, 1);
 
@@ -144,9 +169,9 @@ int dump_buffer_to_ipfix(uint16_t template_n_id){
 }
 
 /*
- * Sendet den inhalt eines (proc)files an ipfix
+ * Sendet den Inhalt eines (proc)files an ipfix
  *
- * ACHTUNG: noch kein template handling implementiert! *
+ * ACHTUNG: noch kein template Handling implementiert! *
  * Bis jetzt wird immer mit template id 0 gesendet, was nicht klappen sollte ;)
  */
 void file_to_ipfix(char* filename,regex_t* regEx,transform_rule* rules, int num_rules){
@@ -156,21 +181,19 @@ void file_to_ipfix(char* filename,regex_t* regEx,transform_rule* rules, int num_
 		fprintf(stderr, "Reading from /proc-System failed!\n");
 		exit(-1);
 	}
-
-
 	//Lines durchgehen, matchen und verschicken
 	int in_line;
-	char cur_line[MAX_LINE_LENGTH];
-	for (in_line = 0; fgets(cur_line, MAX_LINE_LENGTH, fp) != NULL; in_line++){
+	char curr_line[MAX_LINE_LENGTH];
+	for (in_line = 0; fgets(curr_line, MAX_LINE_LENGTH, fp) != NULL; in_line++){
 
-		//Falls line dem pattern genügt wird sie in den send buffer kopiert und kann
+		//Falls line dem pattern genügt wird sie in den send_buffer kopiert und kann
 		//per ipfix verschickt werden
-		if(line_to_send_buffer(cur_line,regEx,rules,num_rules)){
+		if(line_to_send_buffer(curr_line,regEx,rules,num_rules)){
 
 			//Buffer per ipfix versenden
 			/* ACHTUNG: GEHT NOCH NICHT, DA KEINE TEMPLATES DEFINIERT WURDEN
 			 * FÜR DIE 0 DANN DIE TEMPLATE ID ÜBERGEBEN */
-			dump_buffer_to_ipfix(0);
+			dump_buffer_to_ipfix(my_template_id);
 		}
 
 	}
@@ -205,14 +228,14 @@ int main(int argc, char **argv)
 	//Es werden erstmal nur für die ersten 4 capturing groups regeln erstellt
 	//die erste und dritte wird ignoriert (null regel)
 	//die zweite und vierte wird in 4 byte signed int umgewandelt
-	transform_rule exRules[10];
-	exRules[0] = rule_null;
-	exRules[1] = rule_int;
-	exRules[2] = rule_null;
-	exRules[3] = rule_int;
+	transform_rule ex_rules[10];
+	ex_rules[0] = rule_null;
+	ex_rules[1] = rule_int;
+	ex_rules[2] = rule_null;
+	ex_rules[3] = rule_int;
 
 	//Testen mit test file
-	file_to_ipfix("/home/kami/test-tx",&regEx,exRules,4);
+	file_to_ipfix("./test_data",&regEx,ex_rules,4);
 	return 0;
 }
 
